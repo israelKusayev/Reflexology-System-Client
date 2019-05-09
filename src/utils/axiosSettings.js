b192dd3a5dcc53722d139fbf228e77a465687793
Israel Kusayev <israel.kusayev@gmail.com>
2019-05-09 18:54:00 +0300

    removed console log

--- src/utils/axiosSettings.js
+++ src/utils/axiosSettings.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import history from '../utils/history';
 
-console.log(process.env);
-
 axios.defaults.baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000'
