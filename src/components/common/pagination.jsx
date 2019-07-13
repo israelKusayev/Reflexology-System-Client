10a4a8db214740661941c87edb6e7138954e66cc
Israel Kusayev <israel.kusayev@gmail.com>
2019-07-14 00:14:55 +0300

    pagination next perv button

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -10,16 +10,33 @@ const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
   return (
     <nav>
       <ul className='pagination'>
-        {pages.map(page => (
-          <li
-            key={page}
-            className={page === currentPage ? 'page-item active' : 'page-item'}
+        <li className='page-item'>
+          <span
+            className='page-link'
+            aria-label='Previous'
+            onClick={() => (currentPage > 1 ? onPageChange(currentPage - 1) : null)}
           >
+            <span aria-hidden='true'>&laquo;</span>
+            <span className='sr-only'>Previous</span>
+          </span>
+        </li>
+        {pages.map(page => (
+          <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
             <span className='page-link' onClick={() => onPageChange(page)}>
               {page}
             </span>
           </li>
         ))}
+        <li className='page-item'>
+          <span
+            className='page-link'
+            aria-label='Next'
+            onClick={() => (currentPage < 4 ? onPageChange(currentPage + 1) : null)}
+          >
+            <span aria-hidden='true'>&raquo;</span>
+            <span className='sr-only'>Next</span>
+          </span>
+        </li>
       </ul>
     </nav>
   );
