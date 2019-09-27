6e1483f882d8aca9a417e89af68e0548d9fcef01
Israel Kusayev <israel.kusayev@gmail.com>
2019-09-27 12:51:13 +0300

    new treatment page style

--- src/components/treatment/treatment.jsx
+++ src/components/treatment/treatment.jsx
@@ -13,72 +13,83 @@ const Treatment = ({ treatment, currentPatient, history, isPatientsExists }) => 
   const { visitReason, treatmentNumber, referredBy, findings, recommendations, remarks, reminders, _id } = treatment;
 
   const date = moment(treatment.date);
-  console.log(findings.split('\\n'));
 
   return (
     <>
       <h1 className="text-center bold">טיפול</h1>
-      <h4 className="bold">פרטי לקוח</h4>
-      <p>
-        <PatientName patient={currentPatient} />
-      </p>
+      <div class="card border-dark my-5" style={{ width: '50%', margin: 'auto' }}>
+        <div class="card-header text-center treatment-header">
+          <PatientName patient={currentPatient} />
+        </div>
+        <div class="card-body text-dark">
+          <h4 className="bold">תאריך</h4>
+          <p>
+            {date.format('DD/MM/YYYY')} יום {convertDayToText(date.day())}
+          </p>
+          <hr />
 
-      <h4 className="bold">תאריך</h4>
-      <p>
-        {date.format('DD/MM/YYYY')} יום {convertDayToText(date.day())}
-      </p>
+          {visitReason && (
+            <>
+              <h4 className="bold">סיבת ביקור</h4>
+              <p>{visitReason} </p>
+              <hr />
+            </>
+          )}
 
-      {visitReason && (
-        <>
-          <h4 className="bold">סיבת ביקור</h4>
-          <p>{visitReason} </p>
-        </>
-      )}
+          <>
+            <h4 className="bold">מספר טיפול</h4>
+            <p>{treatmentNumber} </p>
+            <hr />
+          </>
 
-      <>
-        <h4 className="bold">מספר טיפול</h4>
-        <p>{treatmentNumber} </p>
-      </>
+          {referredBy && (
+            <>
+              <h4 className="bold">הופנה ע"י</h4>
+              <p>{referredBy} </p>
+              <hr />
+            </>
+          )}
 
-      {referredBy && (
-        <>
-          <h4 className="bold">הופנה ע"י</h4>
-          <p>{referredBy} </p>
-        </>
-      )}
+          {findings && (
+            <>
+              <h4 className="bold">ממצאים</h4>
+              <p>{findings} </p>
+              <hr />
+            </>
+          )}
 
-      {findings && (
-        <>
-          <h4 className="bold">ממצאים</h4>
-          <p>{findings} </p>
-        </>
-      )}
+          {recommendations && (
+            <>
+              <h4 className="bold">המלצות</h4>
+              <p>{recommendations} </p>
+              <hr />
+            </>
+          )}
 
-      {recommendations && (
-        <>
-          <h4 className="bold">המלצות</h4>
-          <p>{recommendations} </p>
-        </>
-      )}
+          {/* ישן */}
+          {remarks && (
+            <>
+              <h4 className="bold">הערות</h4>
+              <p>{remarks} </p>
+              <hr />
+            </>
+          )}
 
-      {/* ישן */}
-      {remarks && (
-        <>
-          <h4 className="bold">הערות</h4>
-          <p>{remarks} </p>
-        </>
-      )}
-
-      {reminders && (
-        <>
-          <h4 className="bold">תזכורות</h4>
-          <p>{reminders} </p>
-        </>
-      )}
-
-      <button className="btn btn-outline-secondary" onClick={() => history.push('/edit-treatment/' + _id)}>
-        ערוך
-      </button>
+          {reminders && (
+            <>
+              <h4 className="bold">תזכורות</h4>
+              <p>{reminders} </p>
+              <hr />
+            </>
+          )}
+          <button
+            className="btn btn-outline-secondary btn-block"
+            onClick={() => history.push('/edit-treatment/' + _id)}
+          >
+            ערוך
+          </button>
+        </div>
+      </div>
     </>
   );
 };
