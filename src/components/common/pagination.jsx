01caddc14fa394fd32da52fe7ea1a5adb3b39d94
Israel Kusayev <israel.kusayev@gmail.com>
2019-12-07 20:24:45 +0200

    :sparkles: pagination

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,44 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import ReactJsPagination from 'react-js-pagination';
 
 const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
-  const pagesCount = Math.ceil(itemsCount / pageSize);
-  if (pagesCount === 1) return null;
-  const pages = _.range(1, pagesCount + 1);
 
   return (
-    <nav>
-      <ul className="pagination">
-        <li className="page-item">
-          <span
-            className="page-link"
-            aria-label="Previous"
-            onClick={() => (currentPage > 1 ? onPageChange(currentPage - 1) : null)}
-          >
-            <span aria-hidden="true">&laquo;</span>
-            <span className="sr-only">Previous</span>
-          </span>
-        </li>
-        {pages.map(page => (
-          <li key={page} className={page === currentPage ? 'page-item active' : 'page-item'}>
-            <span className="page-link" onClick={() => onPageChange(page)}>
-              {page}
-            </span>
-          </li>
-        ))}
-        <li className="page-item">
-          <span
-            className="page-link"
-            aria-label="Next"
-            onClick={() => (currentPage < 4 ? onPageChange(currentPage + 1) : null)}
-          >
-            <span aria-hidden="true">&raquo;</span>
-            <span className="sr-only">Next</span>
-          </span>
-        </li>
-      </ul>
-    </nav>
+    <ReactJsPagination
+      activePage={currentPage}
+      itemsCountPerPage={pageSize}
+      totalItemsCount={itemsCount}
+      pageRangeDisplayed={5}
+      onChange={onPageChange}
+      itemClass="page-item"
+      activeClass="active"
+      linkClass="page-link"
+      nextPageText="&rsaquo;"
+      prevPageText="&lsaquo;"
+    />
   );
 };
 
